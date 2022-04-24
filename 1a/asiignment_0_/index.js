d58801bebe879cc7e6b7353fc5122f9bf7695f6e
Anuj Pandey <anuj.pandey@kaksha.ai>
2022-04-25 01:34:05 +0530

    Demonstrating Git

---
+++ 1a/asiignment_0_/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#enquirymodal").modal('show');
+});
+
+
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  });
+  
